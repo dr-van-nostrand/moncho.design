adb548a5c392fd181b0292d0325978377ebf451f
Lucas Zubeldia <lzubeldia@gmail.com>
2021-04-27 20:38:13 -0300

    5. FAQ Section

---
+++ src/components/FaqSection.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+let FaqSection = (props) => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How do I Start?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum,
+            autem!
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Daily Schedule</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum,
+            autem!
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Payments</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum,
+            autem!
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>What Product do you Offer?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum,
+            autem!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -2,12 +2,14 @@ import React from "react";
 //import page component
 import AboutSection from "../components/AboutSection";
 import ServicesSection from "../components/ServicesSection";
+import FaqSection from "../components/FaqSection";
 
 let AboutUs = (props) => {
   return (
     <>
       <AboutSection />;
       <ServicesSection />
+      <FaqSection />
     </>
   );
 };
